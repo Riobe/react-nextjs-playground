ddd44de6960d34a550dec20be2848d0c7af8e38c
Jeremy Pridemore <jeremy.pridemore@allegion.com>
2026-03-08 18:42:20 -0500

    Fully test relevant bits of the like button

--- app/like-button.spec.tsx
+++ app/like-button.spec.tsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import LikeButton from './like-button';
 
 describe('Like Button', () => {
-  it('renders a button', () => {
+  it('renders a button', async () => {
     render(<LikeButton />);
 
     const button = screen.getByRole('button');
@@ -11,7 +12,38 @@ describe('Like Button', () => {
     expect(button).toBeInTheDocument();
   });
 
-  it('renders the Like Button unchanged', () => {
+  it('starts with 0 likes', async () => {
+    render(<LikeButton />);
+
+    const button = screen.getByRole('button');
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('(0)');
+  });
+
+  it('increases the number when clicked', async () => {
+    render(<LikeButton />);
+    const user = userEvent.setup();
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+    expect(button).toHaveTextContent('(1)');
+  });
+
+  it('changes color when hovered over', async () => {
+    render(<LikeButton />);
+    const user = userEvent.setup();
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+    expect(button).toHaveTextContent('(1)');
+  });
+
+  it('renders the Like Button unchanged', async () => {
     const { container } = render(<LikeButton />);
     expect(container).toMatchSnapshot();
   });
