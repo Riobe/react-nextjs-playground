7a373d0501cdf45666e4f620e9c32404a8c155a0
Jeremy Pridemore <jeremy.pridemore@allegion.com>
2026-03-08 18:45:28 -0500

    Initial add of storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,33 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import pluginJest from "eslint-plugin-jest";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    // update this to match your test files
-    // files: ["**/*.spec.js", "**/*.test.js"],
-    // plugins: { jest: pluginJest },
-    // languageOptions: {
-    //   globals: pluginJest.environments.globals.globals,
-    // },
-    rules: {
-      quotes: ["warn", "single"],
-      // "jest/no-disabled-tests": "warn",
-      // "jest/no-focused-tests": "error",
-      // "jest/no-identical-title": "error",
-      // "jest/prefer-to-have-length": "warn",
-      // "jest/valid-expect": "error",
-    },
+const eslintConfig = defineConfig([...nextVitals, ...nextTs, {
+  // update this to match your test files
+  // files: ["**/*.spec.js", "**/*.test.js"],
+  // plugins: { jest: pluginJest },
+  // languageOptions: {
+  //   globals: pluginJest.environments.globals.globals,
+  // },
+  rules: {
+    quotes: ["warn", "single"],
+    // "jest/no-disabled-tests": "warn",
+    // "jest/no-focused-tests": "error",
+    // "jest/no-identical-title": "error",
+    // "jest/prefer-to-have-length": "warn",
+    // "jest/valid-expect": "error",
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+}, // Override default ignores of eslint-config-next.
+globalIgnores([
+  // Default ignores of eslint-config-next:
+  ".next/**",
+  "out/**",
+  "build/**",
+  "next-env.d.ts",
+]), ...storybook.configs["flat/recommended"]]);
 
 export default eslintConfig;
